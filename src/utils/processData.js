fc727ac394ffd98f79a9b51757d5b5fa2efa1b40
OussamaAlouat <al317257@uji.es>
2018-12-05 07:57:29 +0100

    Add all postalcodes on one array

---
+++ src/utils/processData.js
@@ -0,0 +1,31 @@
+const processDataUsingPostalCode = (result, req, res, next) => {
+
+    const postalCodes = result.reduce((acc, val) => {
+        return acc.concat({latitude: val.latitude, longitude: val.longitude, postalCode: val.postal_code});
+    }, []);
+
+    const processData = result.reduce((acc, val) => {
+        const {postal_code, ...rest} = val;
+        if (!isInTheArray(val, acc)) {
+            const codes = postalCodes.filter((code) => code.latitude === val.latitude && code.longitude === val.longitude);
+            const codesWithoutLatitudeAndLongitude = getPostalsWithoutLatitudeAndLongitude(codes);
+            return acc.concat(Object.assign({}, rest, {postalCodes: codesWithoutLatitudeAndLongitude}))
+        }
+        return acc
+    }, []);
+
+
+    next(processData)
+};
+
+const isInTheArray = (city, cityArrays) => {
+    return cityArrays.filter(val => val.latitude === city.latitude && val.longitude === city.longitude).length > 0;
+};
+
+const getPostalsWithoutLatitudeAndLongitude = (postals) => {
+  return  postals.reduce((acc, val) => {
+        return acc.concat(val.postalCode)
+    }, [])
+};
+
+export {processDataUsingPostalCode}
